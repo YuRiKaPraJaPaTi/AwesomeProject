491c234557ff7ad4a0bd473f9b4cc987ac4bfb8e
YURIKA PRAJAPATI <prajapatiyurika@gmail.com>
2025-07-09 09:41:56 +0545

    edit delete icon, multiline task

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -2,6 +2,7 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import MyCheckbox from './MyCheckbox'
 import { Todo } from '../screens/TodoScreen'
+import SocialIcon from './SocialIcon'
 
 interface TodoItemProps {
       todo: Todo;
@@ -12,10 +13,27 @@ interface TodoItemProps {
 const TodoItem = ({todo, onToggle}: TodoItemProps) => {
   return (
     <View style={styles.container}>
-      <MyCheckbox completed={todo.completed} onToggle={onToggle} />
-      <Text style={[styles.text, todo.completed && styles.completed]}>
-        {todo.task}
-      </Text>
+            <MyCheckbox completed={todo.completed} onToggle={onToggle} />
+            
+            <View style={styles.textContainer}>
+                  <Text 
+                        // numberOfLines={2}
+                        // ellipsizeMode='tail'
+                        style={[styles.text, todo.completed && styles.completed]}>
+                  {todo.task}
+                  </Text>
+            </View>
+
+            <View style={styles.iconContainer}>
+                  <SocialIcon 
+                        source={require('../../assets/edit.png')}
+                        size={22}
+                  />
+                  <SocialIcon 
+                        source={require('../../assets/delete.png')}
+                        size={22}
+                  />
+            </View>
     </View>
   )
 }
@@ -24,11 +42,19 @@ export default TodoItem
 
 const styles = StyleSheet.create({
       container: {
+            flex: 1,
+            gap: 7,
             flexDirection: 'row',
-            alignItems: 'center',
+            alignItems: 'flex-start',
             // backgroundColor: 'red',
-            
       },
+
+      textContainer: {
+            flex:1,
+            // marginRight: 10,
+            // backgroundColor: 'red',
+      },
+
       text: {
             fontSize: 18,
             color: '#333',
@@ -36,5 +62,9 @@ const styles = StyleSheet.create({
       completed: {
             textDecorationLine: 'line-through',
             color: 'green',
+      },
+      iconContainer: {
+            flexDirection: 'row',
+            alignItems: 'center',
       },
 })
